e403d922c5aaf5d00026b4e1b61f64dd913ab829
Joshua Hughes <joshua@plutonus.dev>
2024-04-06 02:19:36 +1000

    refactor: rename Class1.cs to vatACARS.cs
    Renaming of Class1.cs to vatACARS.cs for better clarity of class functionality. Also, updated vatACARS.csproj to reflect these changes.

---
+++ vatACARS/vatACARS.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using vatsys;
+using vatsys.Plugin;
+
+namespace vatACARS
+{
+    [Export(typeof(IPlugin))]
+    public class vatACARS : IPlugin
+    {
+        public string Name { get => "vatACARS"; }
+
+        private CustomToolStripMenuItem acarsWindowMenu;
+
+        public vatACARS()
+        {
+            // Startup code here.
+            return;
+        }
+
+        public void OnFDRUpdate(FDP2.FDR updated) { }
+
+        public void OnRadarTrackUpdate(RDP.RadarTrack updated) { }
+    }
+}
