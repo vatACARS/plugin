ccfbbd132a6cc9c381c1ca674e3418e0880a4fb5
Joshua Hughes <joshua@plutonus.dev>
2024-04-14 16:03:01 +1000

    feat: add audio and uplink interfaces and enhance http client
    This commit includes the addition of the AudioInterface and UplinkInterface classes. The AudioInterface class provides methods for playing sound files, while the UplinkInterface class provides a structure for storing uplink entries.
    
    Moreover, this commit includes significant enhancements to the HttpClient class. Specifically, methods for GET, POST, and file download requests have been refactored and improved, including better error handling and logging functionality.

---
+++ vatACARS/Util/UplinkInterface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace vatACARS.Util
+{
+    [XmlRoot("data")]
+    public class UplinkInterface
+    {
+        [XmlElement("Entry")]
+        public List<UplinkEntry> Entries { get; set; }
+    }
+
+    public class UplinkEntry
+    {
+        [XmlAttribute("Code")]
+        public string Code { get; set; }
+
+        [XmlAttribute("Element")]
+        public string Element { get; set; }
+
+        [XmlAttribute("Response")]
+        public string Response { get; set; }
+
+        [XmlAttribute("Group")]
+        public string Group { get; set; }
+    }
+}
