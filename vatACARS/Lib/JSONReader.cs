fef4d254ac461a1d5391260b942d125963393260
Joshua Hughes <joshua@plutonus.dev>
2024-04-21 02:48:41 +1000

    refactor: rename Uplinks to XMLReader and add JSONReader
    Renamed the "Uplinks" class to "XMLReader" to make its purpose more clear, and added a new "JSONReader" class for files. Also removed the unused "UplinkInterface" class. No changes to logic were made.

---
+++ vatACARS/Lib/JSONReader.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using vatACARS.Util;
+
+namespace vatACARS.Lib
+{
+    public static class JSONReader
+    {
+        private static Logger logger = new Logger("JSONReader");
+        private static string dirPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\vatACARS";
+        public static dynamic quickFillItems;
+
+        public static void MakeQuickFillItems()
+        {
+            try
+            {
+                logger.Log("Reading quickfill items...");
+
+                logger.Log("Deserializing...");
+                string jsonString = File.ReadAllText($"{dirPath}\\data\\quickfill.json");
+                quickFillItems = JsonConvert.DeserializeObject(jsonString);
+                logger.Log("Done!");
+            } catch (Exception ex)
+            {
+                logger.Log($"Something went wrong!\n{ex.ToString()}");
+            }
+        }
+    }
+}
