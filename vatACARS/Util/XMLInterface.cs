5e9f0992dfcf882fda4713a82e4a7199ad8e5759
Joshua Hughes <joshua@plutonus.dev>
2024-04-21 02:49:02 +1000

    feat: add JSON and XML interface classes
    This commit adds two new classes for interfacing with JSON and XML data formats. The JSONInterface class has been created but is currently blank, while the XMLInterface class has been partially implemented with attributes for parsing XML elements.

---
+++ vatACARS/Util/XMLInterface.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace vatACARS.Util
+{
+    [XmlRoot("data")]
+    public class XMLInterface
+    {
+        [XmlElement("ENTRY")]
+        public List<UplinkEntry> Entries { get; set; }
+    }
+
+    public class UplinkEntry
+    {
+        [XmlAttribute("CODE")]
+        public string Code { get; set; }
+
+        [XmlAttribute("MESSAGE")]
+        public string Element { get; set; }
+
+        [XmlAttribute("RESP")]
+        public string Response { get; set; }
+
+        [XmlAttribute("GROUP")]
+        public string Group { get; set; }
+
+        // TODO: "URG" & "ALRT" elems
+    }
+}
