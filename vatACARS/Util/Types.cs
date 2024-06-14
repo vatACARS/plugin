930d26e5b8322ec1e089da912ce4a0764e28c6ec
Joshua Hughes <joshua@plutonus.dev>
2024-06-14 16:04:22 +1000

    feat: add utility types for API responses and station information
    This commit introduces new classes for handling API responses and storing station information. These classes will be used to simplify interactions with the VATSIM network and improve code readability.

---
+++ vatACARS/Util/Types.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using vatsys;
+
+namespace vatACARS.Util
+{
+    public class APIResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class StationInformation
+    {
+        public string Id { get; set; }
+        public string Station_Code { get; set; }
+        public DateTime Opened { get; set; }
+        public string Sectors { get; set; }
+        public string ApproxLoc { get; set; }
+        public string Cid { get; set; }
+    }
+
+    public class LoginResponse : APIResponse
+    {
+        public StationInformation ATSU { get; set; }
+    }
+
+    public class Sector
+    {
+        public string Name { get; set; }
+        public string Callsign { get; set; }
+        public string Frequency { get; set; }
+    }
+
+    public class LatLon {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
